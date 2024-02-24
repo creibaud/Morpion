dc80e75b03f821fcc56bf18acdb4e883256af2fc
creibaud <creibaud@guardiaschool.fr>
2024-02-24 18:41:37 +0100

    Update code formatting and fix minor bugs

---
+++ src/game.py
@@ -0,0 +1,39 @@
+from src.grid import Grid
+from src.panel import Panel
+from src.line import Line
+
+class Game:
+    def __init__(self):
+        self.grid = Grid()
+        self.panel = Panel()
+        self.line = Line()
+
+        self.player = "X"
+        self.gameOver = False
+
+    def handleMouseHover(self, pos):
+        self.grid.handleMouseHover(pos, self.gameOver)
+    
+    def handleMouseClick(self, pos):
+        if self.grid.handleMouseClick(pos, self.player, self.gameOver):
+            self.isGameOver()
+            self.player = "O" if self.player == "X" else "X"
+            self.panel.update(self.player)
+    
+    def isGameOver(self):
+        winner = self.grid.checkWinner(self.player)
+        if winner:
+            self.panel.winner = self.player
+            self.gameOver = True
+        else:
+            tie = self.grid.isTie()
+            if tie:
+                self.panel.tie = True
+                self.gameOver = True
+
+    def draw(self, screen):
+        self.grid.draw(screen)
+        self.panel.draw(screen)
+
+        if self.gameOver:
+            self.line.draw(screen, self.grid.grid)
---
+++ src/grid.py
@@ -0,0 +1,69 @@
+from src.constants import *
+from src.cell import Cell
+
+class Grid:
+    def __init__(self):
+        self.grid = self.initGrid()
+        self.cells = self.initCells()
+
+    def initGrid(self):
+        grid = []
+        for row in range(ROWS):
+            grid.append([])
+            for col in range(COLS):
+                grid[row].append("")
+        return grid
+    
+    def initCells(self):
+        cells = []
+        for row in range(ROWS):
+            cells.append([])
+            for col in range(COLS):
+                cells[row].append(Cell(row, col, self.grid[row][col]))
+        return cells
+    
+    def handleMouseHover(self, pos, gameOver):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.cells[row][col].rect.collidepoint(pos) and self.grid[row][col] == "" and not gameOver:
+                    self.cells[row][col].color = CELL_BG_HOVER
+                else:
+                    self.cells[row][col].color = CELL_BG
+    
+    def handleMouseClick(self, pos, player, gameOver):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.cells[row][col].rect.collidepoint(pos) and self.grid[row][col] == "" and not gameOver:
+                    self.grid[row][col] = player
+                    self.cells[row][col].content = player
+                    return True
+        return False
+    
+    def checkWinner(self, player):
+        for row in range(ROWS):
+            if [self.grid[row][col] for col in range(COLS)] == [player] * COLS:
+                return True
+            
+        for col in range(COLS):
+            if [self.grid[row][col] for row in range(ROWS)] == [player] * ROWS:
+                return True
+            
+        if [self.grid[i][i] for i in range(ROWS)] == [player] * ROWS:
+            return True
+        
+        if [self.grid[i][ROWS - 1 - i] for i in range(ROWS)] == [player] * ROWS:
+            return True
+        
+        return False
+
+    def isTie(self):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.grid[row][col] == "":
+                    return False
+        return True
+    
+    def draw(self, screen):
+        for row in range(ROWS):
+            for col in range(COLS):
+                self.cells[row][col].draw(screen)
